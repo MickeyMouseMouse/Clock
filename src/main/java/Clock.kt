fd8921dc25c79f34d6cd87aedf05ca7049f987f6
xb <xboooox123@gmail.com>
2019-06-10 22:35:26 +0300

    Clock App

---
+++ src/main/java/Clock.kt
@@ -0,0 +1,161 @@
+import javafx.animation.AnimationTimer
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.image.Image
+import javafx.scene.layout.Pane
+import javafx.scene.paint.Color
+import javafx.scene.text.Font
+import javafx.stage.Stage
+import javafx.scene.input.KeyCode
+import javafx.scene.text.TextAlignment
+
+// Визуализация стрелочных часов (системное время/настройка времени)
+class Clock : Application() {
+    private val controller = Controller()
+
+    override fun start(stage: Stage) {
+        // установка иконки приложения
+        stage.icons.add(Image("icon.png"))
+
+        // настройка окна
+        stage.title = "Clock"
+        stage.isResizable = false
+
+        // минутная стрелка
+        val pictureMinuteHand = controller.minuteHand.graphicsContext2D
+        val minuteX = doubleArrayOf(5.0, 15.0, 25.0, 20.0, 20.0, 10.0, 10.0)
+        val minuteY = doubleArrayOf(30.0, 0.0, 30.0, 30.0, 160.0, 160.0, 30.0)
+        pictureMinuteHand.strokePolygon(minuteX, minuteY,minuteX.size)
+        controller.minuteHand.layoutX = 230.0
+        controller.minuteHand.layoutY = 75.0
+
+        // часовая стрелка
+        val pictureHourHand = controller.hourHand.graphicsContext2D
+        val hourX = doubleArrayOf(0.0, 15.0, 30.0, 20.0, 20.0, 10.0, 10.0)
+        val hourY = doubleArrayOf(30.0, 0.0, 30.0, 30.0, 100.0, 100.0, 30.0)
+        pictureHourHand.fillPolygon(hourX, hourY, hourX.size)
+        controller.hourHand.layoutX = 230.0
+        controller.hourHand.layoutY = 135.0
+
+        // "пимпочка"
+        controller.circle.fill = Color.DIMGREY
+        controller.circle.centerX = 244.0
+        controller.circle.centerY = 238.0
+
+        // надпись со временем
+        controller.labelTime.layoutX = 400.0
+        controller.labelTime.layoutY = 20.0
+        controller.labelTime.font = Font(20.0)
+
+        // кнопка "Info"
+        controller.buttonInfo.layoutX = 425.0
+        controller.buttonInfo.layoutY = 440.0
+
+        // надпись, указывающая на то, что показывается установленное пользователем время
+        controller.labelUserTime.layoutX = 15.0
+        controller.labelUserTime.layoutY = 20.0
+        controller.labelUserTime.font = Font(20.0)
+        controller.labelUserTime.textFill = Color.RED
+        controller.labelUserTime.isVisible = false
+
+        // создание главных Панели и Сцены
+        val pane = Pane()
+        val scene = Scene(pane, 480.0,480.0)
+
+        // добавление элементов на сцену и показ
+        pane.children.add(controller.image)
+        pane.children.add(controller.minuteHand)
+        pane.children.add(controller.hourHand)
+        pane.children.add(controller.circle)
+        pane.children.add(controller.labelTime)
+        pane.children.add(controller.buttonInfo)
+        pane.children.add(controller.labelUserTime)
+        stage.scene = scene
+        stage.show()
+
+
+        // еще одна панель и сцена для вывода информации
+        val paneInfo = Pane()
+        val sceneInfo = Scene(paneInfo, 480.0,480.0)
+
+        // надпись с информацией 1
+        controller.labelInfo1.layoutX = 70.0
+        controller.labelInfo1.layoutY = 80.0
+        controller.labelInfo1.font = Font(20.0)
+        controller.labelInfo1.isWrapText = true
+        controller.labelInfo1.maxWidth = 340.0
+        controller.labelInfo1.textAlignment = TextAlignment.CENTER
+
+        // надпись с информацией 2
+        controller.labelInfo2.layoutX = 50.0
+        controller.labelInfo2.layoutY = 250.0
+        controller.labelInfo2.font = Font(20.0)
+        controller.labelInfo2.isWrapText = true
+        controller.labelInfo2.maxWidth = 375.0
+        controller.labelInfo2.textAlignment = TextAlignment.CENTER
+
+        // кнопка "Back"
+        controller.buttonBack.layoutX = 420.0
+        controller.buttonBack.layoutY = 440.0
+
+        paneInfo.children.add(controller.labelInfo1)
+        paneInfo.children.add(controller.labelInfo2)
+        paneInfo.children.add(controller.buttonBack)
+
+
+        // таймер для перерисовки интерфейса
+        object: AnimationTimer() {
+            override fun handle(now: Long) {
+                controller.update()
+            }
+        }.start()
+
+        // события на нажатия кнопок клавиатуры (H, M, R)
+        scene.setOnKeyPressed {
+            when (it.code) {
+                // показать реальное время
+                KeyCode.R -> {
+                    controller.labelUserTime.isVisible = false
+                    controller.fl = true
+                }
+
+                // плюс один час
+                KeyCode.H -> {
+                    controller.labelUserTime.isVisible = true
+                    controller.fl = false
+
+                    if (++controller.hours == 12) {
+                        controller.hours = 0
+                    }
+                }
+
+                // плюс одна минута
+                KeyCode.M -> {
+                    controller.labelUserTime.isVisible = true
+                    controller.fl = false
+
+                    if (++controller.minutes == 60) {
+                        controller.minutes = 0
+                        if (++controller.hours == 12)
+                            controller.hours = 0
+                    }
+                }
+
+                else -> {}
+            }
+        }
+
+        // показать вторую сцену с информацией
+        controller.buttonInfo.setOnMouseClicked { stage.scene = sceneInfo }
+
+        // закрыть вторую сцену
+        controller.buttonBack.setOnMouseClicked { stage.scene = scene }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            launch(Clock::class.java)
+        }
+    }
+}
---
+++ src/main/java/Controller.kt
@@ -0,0 +1,64 @@
+import javafx.scene.canvas.Canvas
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.image.ImageView
+import javafx.scene.shape.Circle
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class Controller {
+    // циферблат
+    val image = ImageView("clock.png")
+
+    // минутная стрелка
+    val minuteHand = Canvas(30.0, 320.0)
+
+    // часовая стрелка
+    val hourHand = Canvas(30.0, 200.0)
+
+    // "пимпочка"
+    val circle = Circle(17.0)
+
+    // надпись со временем
+    val labelTime = Label()
+
+    // кнопка "Info"
+    val buttonInfo = Button("Info")
+
+    // кнопка "Close"
+    val buttonBack = Button("Back")
+
+    // надпись с информацией 1
+    val labelInfo1 = Label("Application \"Clock\" by Andrew Jeus Version 1.0 (10 June 2019)")
+
+    // надпись с информацией 2
+    val labelInfo2 = Label("You can adjust the time manually: press H to adjust the number of hours; press M to adjust the number of minutes; press R to see the real time.")
+
+    // надпись, указывающая на то, что показывается установленное пользователем время
+    val labelUserTime = Label("User Time")
+
+    // true - показывать реальное время
+    // false - показывать пользовательское время
+    var fl = true
+
+    // время, которое нужно показать стрелками
+    var hours = 0
+    var minutes = 0
+
+    fun update() {
+        if (fl) {
+            hours = LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("hh")).toInt() % 12
+            minutes = LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("mm")).toInt() % 60
+        }
+
+        // 1 минута = 6 градусов
+        minuteHand.rotate = 6.0 * minutes
+        // 1 час = 30 градусов (5 минут) + добавка
+        hourHand.rotate = 30.0 * hours + minutes / 2
+
+        // обновление индикации времени в label
+        labelTime.text = "$hours:$minutes"
+        }
+}
